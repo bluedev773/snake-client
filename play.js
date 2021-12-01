6cec2c58d0c50d2b517ac54ecb82dec7369432a5
Matthew Litvinov <matthew.litvinov@gmail.com>
2021-12-01 14:09:30 -0500

    close with ctrl c

--- play.js
+++ play.js
@@ -1,7 +1,22 @@
 
 const connect = require('./client');
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
 
+  stdin.on("data", handleUserInput);
+
+  const handleUserInput = function(key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  }
+
+  return stdin;
+};
 
 console.log("Connecting ...");
 connect();
